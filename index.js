557bf55862439173e469b233504f5b7f019a2de5
Mark Chapman <mark.chapman@gmail.com>
2015-05-22 12:15:22 +0100

    Trying to make it work outside EC2

--- index.js
+++ index.js
@@ -8,62 +8,112 @@ var configDone = false;
 
 module.exports = {};
 
-module.exports.getInstanceId = function(cb) {
-  if (instanceId) {
-    cb(null, instanceId);
-  } else {
-    http.get({host:'169.254.169.254', port:80, path:'/latest/meta-data/instance-id'}, function(res){
-      res.on('data', function (chunk) {
-        instanceId = chunk.toString();
-        cb(null, instanceId);
-      }).on('error', function(e) {
-        cb(e);
-      });
+module.exports.getInstanceId = function (cb) {
+
+  // Instanceid is actually detected by onEC2
+
+  function returnInstanceId() {
+    if (instanceId) {
+      cb(null, instanceId);
+    } else {
+      cb(new Error('You cannot call getInstanceId unless you ar on EC2 instance'))
+    }
+  }
+
+  if (typeof instanceId === 'undefined') {
+    module.exports.onEC2(function (err) {
+      if (err) {
+        cb(err);
+      }
+      returnInstanceId();
     });
+  } else {
+    returnInstanceId();
   }
 };
 
-module.exports.configure = function(file) {
+module.exports.onEC2 = function (cb) {
+  if (instanceId) {
+    cb(null, instanceId);
+  } else {
+    var options = {host: '169.254.169.254', port: 80, path: '/latest/meta-data/instance-id'};
+
+    var req = http.request(options, function (res) {
+      res.on('data', function (chunk) {
+        instanceId = chunk.toString();
+        cb(null, instanceId);
+      }).on('error', function (e) {
+        console.log('here');
+        cb(e);
+      });
+    });
+
+    req.on('socket', function (socket) {
+      socket.setTimeout(500);
+      socket.on('timeout', function () {
+        instanceId = false;
+        cb(null, false);
+        req.abort();
+      }).on('error', function(err) {
+        console.log(err);
+      });
+    });
+
+    req.end();
+  }
+};
+
+module.exports.configure = function (file) {
   require('util')._extend(AWS.config, require(file));
   if (!AWS.config.region) {
     // Default to Virginia
     AWS.config.region = 'us-east-1';
 
     // But attempt to find the actual region of the instance
-    http.get({host:'169.254.169.254', port:80, path:'/latest/meta-data/placement/availability-zone'}, function(res){
-      res.on('data', function(chunk) {
-        AWS.config.region = chunk.toString().slice(0,-1);
+    http.get({
+      host: '169.254.169.254',
+      port: 80,
+      path: '/latest/meta-data/placement/availability-zone'
+    }, function (res) {
+      res.on('data', function (chunk) {
+        AWS.config.region = chunk.toString().slice(0, -1);
       });
     });
   }
   configDone = true;
 };
 
-module.exports.getLoadBalancers = function(config, cb) {
-  if (!configDone) { cb(new Error("No AWS config present")); }
+module.exports.getLoadBalancers = function (config, cb) {
+  if (!configDone) {
+    cb(new Error("No AWS config present"));
+  }
   if (!config.region) {
     config.region = AWS.config.region;
   }
   elb = elb || new AWS.ELB();
-  elb.describeLoadBalancers({}, function(err, data) {
+  elb.describeLoadBalancers({}, function (err, data) {
     if (err) {
       cb(err);
     } else {
-      cb(null,data.LoadBalancerDescriptions);
+      cb(null, data.LoadBalancerDescriptions);
     }
   });
 };
 
-module.exports.amIFirst = function(cb) {
-  module.exports.getInstanceId(function (err,instance) {
-    if (err) {cb(err);}
+module.exports.amIFirst = function (cb) {
+  module.exports.getInstanceId(function (err, instance) {
+    if (err) {
+      cb(err);
+    }
     module.exports.getLoadBalancers({}, function (err, lbs) {
-      if (err) {cb(err);}
+      if (err) {
+        cb(err);
+      }
       var thisLB = false;
-      for (var i=0; i< lbs.length;i++) {
+      for (var i = 0; i < lbs.length; i++) {
         var instanceObjects = lbs[i].Instances;
         var instances = [];
-        for (var j=0 ; j < instanceObjects.length ; j++) {
+        for (var j = 0; j < instanceObjects.length; j++) {
           var thisInstance = instanceObjects[j].InstanceId;
           if (thisInstance === instance) {
             thisLB = true;
@@ -76,9 +126,11 @@ module.exports.amIFirst = function(cb) {
             cb(null, true);
           } else {
             ec2 = ec2 || new AWS.EC2();
-            ec2.describeInstanceStatus({InstanceIds:instances}, function(err, data) {
+            ec2.describeInstanceStatus({InstanceIds: instances}, function (err, data) {
               if (err) cb(err);
-              data.InstanceStatuses.sort(function(a,b) {return a.InstanceId < b.InstanceId;});
+              data.InstanceStatuses.sort(function (a, b) {
+                return a.InstanceId < b.InstanceId;
+              });
               cb(null, (data.InstanceStatuses[0].InstanceId === instance));
             });
           }
@@ -92,7 +144,7 @@ module.exports.amIFirst = function(cb) {
   });
 };
 
-module.exports.amIFirstNow = function(firstFunc, notFirstFunc) {
+module.exports.amIFirstNow = function (firstFunc, notFirstFunc) {
 
   var state = 0;
 
--- test.js
+++ test.js
@@ -1,20 +1,30 @@
 var awsUtil = require('./index.js');
 var assert = require('assert');
 
-awsUtil.getInstanceId(function(err, instanceId) {
+awsUtil.onEC2(function(err, onEC2) {
   assert.equal(err, null);
-  assert.equal(typeof instanceId,'string');
+  if (onEC2) {
+    awsUtil.getInstanceId(function(err, instanceId) {
+      assert.equal(err, null);
+      assert.equal(typeof instanceId,'string');
+      console.log(instanceId);
+    });
+
+    awsUtil.configure('./../AWSConfig.js');
+
+    awsUtil.getLoadBalancers({}, function(err, lbs) {
+      assert.equal(err, null);
+      assert(Array.isArray(lbs), 'getLoadBalancers should return an array');
+    });
+
+    awsUtil.amIFirst(function(err, response){
+      assert.equal(response, true, 'I was expecting to be first on the load balancer');
+    });
+    console.log('If you don\'t see any horrible messages and the process finishes you are OK!');
+
+  } else {
+    console.log('You aren\'t running on EC2 instance')
+  }
 });
 
-awsUtil.configure('./../AWSConfig.js');
 
-awsUtil.getLoadBalancers({}, function(err, lbs) {
-  assert.equal(err, null);
-  assert(Array.isArray(lbs), 'getLoadBalancers should return an array');
-});
-
-awsUtil.amIFirst(function(err, response){
-  assert.equal(response, true, 'I was expecting to be first on the load balancer');
-})
-
-console.log('If you can\'t see any horrible messages you are OK!');
